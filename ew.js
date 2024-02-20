4979386d82a4173dc4b5ed93ce0eb37be121f737
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2024-02-20 17:57:36 +0800

    Update ew.js

--- ew.js
+++ ew.js
@@ -1,55 +1,504 @@
-var lastUpdate = Date.now();
-var diff = (Date.now() - lastUpdate) / 1000;
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.25, maximum-scale=4.0, user-scalable=yes" />
+        <title>数据增量</title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+		
+		<style>
+			.automation-button
+			{
+				background-color: red;
+				color: white;
+			}
+		</style>
+	</head>
 
-var player;
-
-function init()
-{
-	player =
-	{
-		energy: new Decimal(10),
-		eb01: new Decimal(0),
-		eb02: new Decimal(0),
-		eb03: new Decimal(0),
-		eb04: new Decimal(0),
-		eb05: new Decimal(0),
-		eb06: new Decimal(0),
-		eb07: new Decimal(0),
-		eb08: new Decimal(0),
-		ebc01: new Decimal(0),
-		ebc02: new Decimal(0),
-		ebc03: new Decimal(0),
-		ebc04: new Decimal(0),
-		ebc05: new Decimal(0),
-		ebc06: new Decimal(0),
-		ebc07: new Decimal(0),
-		ebc08: new Decimal(0)
-	}
-}
-
-function save()
-{
-	localStorage.energyWorld = btoa(JSON.stringify(player));
-	shownoti("#energyWorldAutoSave");
-}
-
-function load()
-{
-	if(!localStorage.energyWorld) return;
-	player = JSON.parse(atob(localStorage.energyWorld));
-	transformToDecimal(player);
-}
-
-function trueHardReset() {
-    let promption = prompt("重置存档？！输入DOIMAKJMHDS确定");
-    if (promption == "DOIMAKJMHDS") {
-        init();
-        shownoti("#resetSave");
-    }
-}
-
-function main()
-{
-	diff = (Date.now() - lastUpdate) / 1000;
-    lastUpdate = Date.now();
-}
+	<body style="background-color: #000000; font-family: MonospaceTypewriter, monospace ">
+		
+		<p align="center" class="ftsz3">
+            <span>你有</span><span class="ftsz5" id="data"></span><span>数据</span>
+        </p>
+		<p align="center" class="ftsz2">
+			<button type="button" onclick="chpage(101)">节点</button>
+			<button type="button" onclick="chpage(201)">硬重置！</button>
+			<button type="button" onclick="chpage(301)">作弊！</button>
+			<br/>
+			<div id="open_automation"><p align="center"><button type="button" class="automation-button" onclick="chpage(102)">自动化</button></p></div>
+		</p>
+		<div id="page101">
+			<p align="center" class="ftsz3">
+				<button type="button" onclick="maxAllUgjd()">全部最大化升级</button>
+			</p>
+			<p align="center" class="ftsz2">
+				<span>你拥有</span><span class="ftsz4" id="speedup"></span><span>个时间加速</span>
+				<button type="button" onclick="buySpeedUp()">购买时间加速升级 花费<span id="speedupcost"></span>数据</button>
+				<span>花费折算：达到1E308花费后，每次购买都会使价格增长速度x10倍</span>
+			</p>
+			<p align="left" class="ftsz3">
+				<span>I型传输节点 </span><span class="ftsz4" id="jd01"></span><span> x</span><span class="ftsz4" id="jdxb01"></span>它们生产数据</span>
+				<button type="button" onclick="ugjd(1)">升级节点 当前花费：<span id="cost01"></span>数据</button>
+				<br/>
+				<span>II型传输节点 </span><span class="ftsz4" id="jd02"></span><span> x</span><span class="ftsz4" id="jdxb02"></span>它们生产I型传输节点</span>
+				<button type="button" onclick="ugjd(2)">升级节点 当前花费：<span id="cost02"></span>数据</button>
+				<br/>
+				<span>III型传输节点 </span><span class="ftsz4" id="jd03"></span><span> x</span><span class="ftsz4" id="jdxb03"></span>它们生产II型传输节点</span>
+				<button type="button" onclick="ugjd(3)">升级节点 当前花费：<span id="cost03"></span>数据</button>
+				<br/>
+				<span>IV型传输节点 </span><span class="ftsz4" id="jd04"></span><span> x</span><span class="ftsz4" id="jdxb04"></span>它们生产III型传输节点</span>
+				<button type="button" onclick="ugjd(4)">升级节点 当前花费：<span id="cost04"></span>数据</button>
+				<br/>
+				<span>V型传输节点 </span><span class="ftsz4" id="jd05"></span><span> x</span><span class="ftsz4" id="jdxb05"></span>它们生产IV型传输节点</span>
+				<button type="button" onclick="ugjd(5)">升级节点 当前花费：<span id="cost05"></span>数据</button>
+				<br/>
+				<span>VI型传输节点 </span><span class="ftsz4" id="jd06"></span><span> x</span><span class="ftsz4" id="jdxb06"></span>它们生产V型传输节点</span>
+				<button type="button" onclick="ugjd(6)">升级节点 当前花费：<span id="cost06"></span>数据</button>
+				<br/>
+				<span>VII型传输节点 </span><span class="ftsz4" id="jd07"></span><span> x</span><span class="ftsz4" id="jdxb07"></span>它们生产VI型传输节点</span>
+				<button type="button" onclick="ugjd(7)">升级节点 当前花费：<span id="cost07"></span>数据</button>
+				<br/>
+				<span>VIII型传输节点 </span><span class="ftsz4" id="jd08"></span><span> x</span><span class="ftsz4" id="jdxb08"></span>它们生产VII型传输节点</span>
+				<button type="button" onclick="ugjd(8)">升级节点 当前花费：<span id="cost08"></span>数据</button>
+				<br/>
+			</p>
+			<p align="center" class="ftsz3">
+				<button type="button" onclick="buyBoost()">
+					跃迁(<span id="boost"></span>)
+					<br/>
+					重置数据和数据传输节点、时间加速升级
+					<br/>
+					为数据传输节点增加<span id="boostx"></span>倍数
+					<br/>
+					需要数据量：<span id="boostcost"></span>
+				</button>
+			</p>
+		</div>
+		<div id="page102">
+			<p align="center" class="ftsz3">
+				<span>自动化·初始层级内容包含：I~VIII型传输节点自动化、时间加速升级自动化</span>
+				<br/>
+				<span class="ftsz2">需要跃迁2次及以上才能开启</span>
+				<br/>
+				<button type="button" onclick="chauto(1)">01节点自动化:<span id="atb01"></span></button>
+				<button type="button" onclick="chauto(2)">02节点自动化:<span id="atb02"></span></button>
+				<button type="button" onclick="chauto(3)">03节点自动化:<span id="atb03"></span></button>
+				<button type="button" onclick="chauto(4)">04节点自动化:<span id="atb04"></span></button>
+				<button type="button" onclick="chauto(5)">05节点自动化:<span id="atb05"></span></button>
+				<button type="button" onclick="chauto(6)">06节点自动化:<span id="atb06"></span></button>
+				<button type="button" onclick="chauto(7)">07节点自动化:<span id="atb07"></span></button>
+				<button type="button" onclick="chauto(8)">08节点自动化:<span id="atb08"></span></button>
+				<br/>
+				<button type="button" onclick="chauto(101)">时间加速自动化:<span id="atbspeedup"></span></button>
+			</p>
+		</div>
+		<div id="page201">
+			<br/>
+			<span class="ftsz2">存档硬重置：毁灭一切</span>
+			<button type="button" onclick="hard_reset()">硬重置</button>
+		</div>
+		<div id="page301">
+			<p align="center" class="ftsz3">
+			<span>作弊：为了您的游戏体验，不建议使用。所以为什么它会出现？emmmmmm</span>
+			<br/>
+			<button type="button" onclick="cheat(600)">时间流逝60秒</button>
+			<br/>
+			<button type="button" onclick="cheat(1200)">时间流逝120秒</button>
+			<br/>
+			<button type="button" onclick="cheat(6000)">时间流逝5分钟</button>
+			<br/>
+			<button type="button" onclick="cheat(12000)">时间流逝10分钟</button>
+			<br/>
+			<button type="button" onclick="cheat(18000)">时间流逝15分钟</button>
+			<br/>
+			<span>时间流逝需要计算一段时间，请耐心等待（仅测试版本有效，上传版本已删除）</span>
+		</div>
+		<script type="text/javascript" src="break_eternity.js"></script>
+		
+		<script>
+			var nowpage = "page101";
+			var player;
+			function init()
+			{
+				player =
+				{
+					data: new Decimal(10),
+					csjd01: new Decimal(0),
+					csjd02: new Decimal(0),
+					csjd03: new Decimal(0),
+					csjd04: new Decimal(0),
+					csjd05: new Decimal(0),
+					csjd06: new Decimal(0),
+					csjd07: new Decimal(0),
+					csjd08: new Decimal(0),
+					jdxb01: new Decimal(1),
+					jdxb02: new Decimal(1),
+					jdxb03: new Decimal(1),
+					jdxb04: new Decimal(1),
+					jdxb05: new Decimal(1),
+					jdxb06: new Decimal(1),
+					jdxb07: new Decimal(1),
+					jdxb08: new Decimal(1),
+					jdx01: new Decimal(1),
+					jdx02: new Decimal(1),
+					jdx03: new Decimal(1),
+					jdx04: new Decimal(1),
+					jdx05: new Decimal(1),
+					jdx06: new Decimal(1),
+					jdx07: new Decimal(1),
+					jdx08: new Decimal(1),
+					cost01: new Decimal(10),
+					cost02: new Decimal(100),
+					cost03: new Decimal(1e4),
+					cost04: new Decimal(1e7),
+					cost05: new Decimal(1e11),
+					cost06: new Decimal(1e16),
+					cost07: new Decimal(1e22),
+					cost08: new Decimal(1e29),
+					costx01: new Decimal(100),
+					costx02: new Decimal(1e4),
+					costx03: new Decimal(1e6),
+					costx04: new Decimal(1e8),
+					costx05: new Decimal(1e10),
+					costx06: new Decimal(1e12),
+					costx07: new Decimal(1e14),
+					costx08: new Decimal(1e16),
+					speedup: new Decimal(0),
+					speedupmul: new Decimal(1.125),
+					speedupcost: new Decimal(1e8),
+					speedupcostx: new Decimal(10),
+					boost: new Decimal(0),
+					boostx: new Decimal(3),
+					boostcost: new Decimal(18446744073709551616),
+					boostcostx: new Decimal(18446744073709551616),
+					atb01: new Decimal(0),
+					atb02: new Decimal(0),
+					atb03: new Decimal(0),
+					atb04: new Decimal(0),
+					atb05: new Decimal(0),
+					atb06: new Decimal(0),
+					atb07: new Decimal(0),
+					atb08: new Decimal(0),
+					atbspeedup: new Decimal(0),
+				};
+			}
+				
+			function hard_reset()
+			{
+				init();
+				data_print();
+			}
+			
+			function notation(number)
+			{
+				var string = "";
+				if (number.sign == -1) {
+					string = string + "-";
+					number.sign = 1;
+				}
+				let power1 = Decimal.floor(Decimal.log10(number));
+				let base1 = number.div(Decimal.pow(10, power1));
+				let power2 = Decimal.floor(Decimal.log10(power1));
+				let base2 = power1.div(Decimal.pow(10, power2));
+				let power3 = Decimal.floor(Decimal.log10(power2));
+				let base3 = power2.div(Decimal.pow(10, power3));
+				if (number == 0) return "0.000";
+				if (power1 < -3) return base.mul(10).toFixed(3) + "e" + power;
+				if (power1 < -2) return number.toFixed(6);
+				if (power1 < -1) return number.toFixed(5);
+				if (power1 < 0) return number.toFixed(4);
+				if (power1 < 1) return number.toFixed(3);
+				if (power1 < 2) return number.toFixed(2);
+				if (power1 < 3) return number.toFixed(1);
+				if (power1 < 6) return number.toFixed(0);
+				if (power1 < 100000000) return base1.toFixed(3) + "e" + power1;
+				if (power2 < 100000000) return "e" + base2.toFixed(3) + "e" + power2;
+				if (power3 < 100000000) return "ee" + base3.toFixed(3) + "e" + power3;
+				return number.mag.toFixed(0) + "f" + number.layer.toFixed(0);
+			}
+			
+			function transformToDecimal(object)
+			{
+				for (i in object) {
+					if (typeof (object[i]) == "string" && !isNaN(new Decimal(object[i]).mag)) object[i] = new Decimal(object[i]);
+					if (typeof (object[i]) == "object" && !isNaN(new Decimal(object[i]).mag)) transformToDecimal(object[i]);
+					if (new Decimal(object[i]).gte("F1e308")) object[i] = new Decimal(1);
+				}
+			}
+			
+			function data_print()
+			{
+				localStorage.QWERTYUIOP = btoa(JSON.stringify(player));
+			}
+			
+			function data_input()
+			{
+				if (!localStorage.QWERTYUIOP) return;
+				player = JSON.parse(atob(localStorage.QWERTYUIOP));
+				transformToDecimal(player);
+			}
+			
+			function chpage(pagename)
+			{
+				nowpage = "page" + pagename;
+			}
+			
+			function ugjd(type)
+			{
+				var costname = "";
+				if(type < 10) costname = "cost0" + String(type);
+				else costname = "cost" + String(type);
+				
+				if(player.data.compare(player[costname]) >= 0)
+				{
+					let jdname = "";
+					if(type < 10) jdname = "csjd0" + String(type);
+					else jdname = "csjd" + String(type);
+					let jdxname = "";
+					if(type < 10) jdxname = "jdx0" + String(type);
+					else jdxname = "jdx" + String(type);
+					let costxname = "";
+					if(type < 10) costxname = "costx0" + String(type);
+					else costxname = "costx" + String(type);
+					player[jdname] = player[jdname].add(new Decimal(10));
+					player.data = player.data.sub(player[costname]);
+					player[jdxname] = player[jdxname].mul(new Decimal(2));
+					player[costname] = player[costname].mul(player[costxname]);
+				}
+			}
+			
+			function buySpeedUp()
+			{
+				if(player.data.compare(player.speedupcost) >= 0)
+				{
+					player.speedup = player.speedup.add(new Decimal(1));
+					player.data = player.data.sub(player.speedupcost);
+					player.speedupcost = player.speedupcost.mul(player.speedupcostx);
+					if(player.speedupcost.compare(new Decimal(1e307)) >= 0)
+					{
+						player.speedupcostx = player.speedupcostx.mul(new Decimal(10));
+					}
+				}
+			}
+			
+			function maxUgjd(type)
+			{
+				let costname = "";
+				if(type < 10) costname = "cost0" + String(type);
+				else costname = "cost" + String(type);
+				while(player.data.compare(player[costname]) >= 0)
+				{
+					ugjd(type);
+				}
+			}
+			
+			function maxAllUgjd(type)
+			{
+				for(let i = 8;i >= 1;i--) maxUgjd(i);
+				while(player.data.compare(player.speedupcost) >= 0) buySpeedUp();
+			}
+			
+			function boostReset()
+			{
+				player.data = new Decimal(10);
+				for(let i = 1;i <= 8;i++)
+				{
+					player["csjd0" + i] = new Decimal(0);
+					player["jdx0" + i] = new Decimal(1);
+					player["jdxb0" + i] = new Decimal(1);
+				}
+				player.cost01 = new Decimal(10);
+				player.cost02 = new Decimal(100);
+				player.cost03 = new Decimal(1e4);
+				player.cost04 = new Decimal(1e7);
+				player.cost05 = new Decimal(1e11);
+				player.cost06 = new Decimal(1e16);
+				player.cost07 = new Decimal(1e22);
+				player.cost08 = new Decimal(1e29);
+				player.costx01 = new Decimal(100);
+				player.costx02 = new Decimal(1e4);
+				player.costx03 = new Decimal(1e6);
+				player.costx04 = new Decimal(1e8);
+				player.costx05 = new Decimal(1e10);
+				player.costx06 = new Decimal(1e12);
+				player.costx07 = new Decimal(1e14);
+				player.costx08 = new Decimal(1e16);
+				player.speedup = new Decimal(0);
+				player.speedupmul = new Decimal(1.125);
+				player.speedupcost = new Decimal(1e8);
+				player.speedupcostx = new Decimal(10);
+			}
+			
+			function buyBoost()
+			{
+				if(player.data.compare(player.boostcost) >= 0)
+				{
+					boostReset();
+					player.boost = player.boost.add(new Decimal(1));
+					player.boostcost = player.boostcost.mul(player.boostcostx);
+				}
+			}
+			
+			function automation_run()
+			{
+				for(let i = 1;i <= 8;i++)
+				{
+					if(player["atb0" + i].compare(new Decimal(1)) == 0)
+					{
+						ugjd(i);
+					}
+				}
+				if(player.atbspeedup.compare(new Decimal(1)) == 0)
+				{
+					buySpeedUp();
+				}
+			}
+			
+			function chauto(type)
+			{
+				if(type >= 1 && type <= 8)
+				{
+					if(player.boost.compare(new Decimal(2)) >= 0)
+					{
+						if(player["atb0" + type].compare(new Decimal(0)) == 0)
+						{
+							player["atb0" + type] = new Decimal(1);
+						}
+						else player["atb0" + type] = new Decimal(0);
+					}
+				}
+				else if(type == 101)
+				{
+					if(player.boost.compare(new Decimal(2)) >= 0)
+					{
+						if(player.atbspeedup.compare(new Decimal(0)) == 0)
+						{
+							player.atbspeedup = new Decimal(1);
+						}
+						else player.atbspeedup = new Decimal(0);
+					}
+				}
+			}
+			
+			function produce()
+			{
+				competejdx();
+				player.data = player.data.add(player.csjd01.mul(player.jdxb01).div(new Decimal(10)));
+				player.csjd01 = player.csjd01.add(player.csjd02.mul(player.jdxb02).div(new Decimal(10)));
+				player.csjd02 = player.csjd02.add(player.csjd03.mul(player.jdxb03).div(new Decimal(10)));
+				player.csjd03 = player.csjd03.add(player.csjd04.mul(player.jdxb04).div(new Decimal(10)));
+				player.csjd04 = player.csjd04.add(player.csjd05.mul(player.jdxb05).div(new Decimal(10)));
+				player.csjd05 = player.csjd05.add(player.csjd06.mul(player.jdxb06).div(new Decimal(10)));
+				player.csjd06 = player.csjd06.add(player.csjd07.mul(player.jdxb07).div(new Decimal(10)));
+				player.csjd07 = player.csjd07.add(player.csjd08.mul(player.jdxb08).div(new Decimal(10)));
+			}
+			
+			function competejdx()
+			{
+				for(let i = 1;i <= 8;i++)
+				{
+					var jdxname = "jdx0" + i, jdxbname = "jdxb0" + i;
+					player[jdxbname] = player[jdxname];
+					player[jdxbname] = player[jdxbname].mul(player.speedupmul.pow(player.speedup));
+					player[jdxbname] = player[jdxbname].mul(player.boostx.pow(player.boost));
+				}
+			}
+			
+			function cheat(time)
+			{
+				//for(let i = 1;i <= time;i++)
+				//{
+				//	produce();
+				//	automation_run();
+				//	GUI();
+				//}
+			}
+			
+			function GUI()
+			{
+				document.getElementById("data").innerHTML = notation(player.data);
+				document.getElementById("speedup").innerHTML = notation(player.speedup);
+				document.getElementById("speedupcost").innerHTML = notation(player.speedupcost);
+				document.getElementById("boost").innerHTML = notation(player.boost);
+				document.getElementById("boostx").innerHTML = notation(player.boostx);
+				document.getElementById("boostcost").innerHTML = notation(player.boostcost);
+				for(let i = 1;i <= 8;i++)
+				{
+					var jdname = "";
+					if(i < 10) jdname = "jd0" + String(i);
+					else jdname = "jd" + String(i);
+					
+					document.getElementById(jdname).innerHTML = notation(player["cs" + jdname]);
+					
+					var costname = "";
+					if(i < 10) costname = "cost0" + i;
+					else costname = "cost" + i;
+					document.getElementById(costname).innerHTML = notation(player[costname]);
+					
+					var jdxname = "";
+					if(i < 10) jdxbname = "jdxb0" + i;
+					else jdxbname = "jdxb" + i;
+					document.getElementById(jdxbname).innerHTML = notation(player[jdxbname]);
+					
+					if(notation(player["atb0" + i]) == "0.000") document.getElementById("atb0" + i).innerHTML = "关";
+					else document.getElementById("atb0" + i).innerHTML = "开";
+				}
+				if(notation(player.atbspeedup) == "0.000") document.getElementById("atbspeedup").innerHTML = "关";
+				else document.getElementById("atbspeedup").innerHTML = "开";
+				if(nowpage != "page101") document.getElementById('page101').style.display = 'none';
+				else document.getElementById('page101').style.display = 'block';
+				if(nowpage != "page102") document.getElementById('page102').style.display = 'none';
+				else document.getElementById('page102').style.display = 'block';
+				if(nowpage != "page201") document.getElementById('page201').style.display = 'none';
+				else document.getElementById('page201').style.display = 'block';
+				if(nowpage != "page301") document.getElementById('page301').style.display = 'none';
+				else document.getElementById('page301').style.display = 'block';
+				if(player.boost.compare(new Decimal(2)) < 0) document.getElementById('open_automation').style.display = 'none';
+				else document.getElementById('open_automation').style.display = 'block';
+			}
+			
+			init();
+			data_input();
+			let playerLen = Object.keys(player).length;
+			console.log(playerLen);
+			if(playerLen < 44)
+			{
+				//远古版本，直接硬重置
+				hard_reset();
+			}
+			else if(playerLen == 44)
+			{
+				player.speedupcostx = new Decimal(10);
+			}
+			else if(playerLen == 45)
+			{
+				player.boost = new Decimal(0);
+				player.boostx = new Decimal(3);
+				player.boostcost = new Decimal(18446744073709551616);
+				player.boostcostx = new Decimal(18446744073709551616);
+			}
+			else if(playerLen == 49)
+			{
+				for(let i = 1;i <= 8;i++)
+				{
+					player["atb0" + i] = new Decimal(0);
+				}
+			}
+			else if(playerLen == 57)
+			{
+				player.atbspeedup = new Decimal(0);
+			}
+			else if(playerLen == 58)
+			{
+				//当前版本
+			}
+			const allPropertyNames = Object.getOwnPropertyNames(player);
+			console.log(allPropertyNames);
+			console.log(atbspeedup);
+			
+			setInterval(produce, 100);
+			setInterval(GUI, 10);
+			setInterval(data_print, 1000);
+			setInterval(automation_run, 100);
+		</script>
+	</body>
+</html>
